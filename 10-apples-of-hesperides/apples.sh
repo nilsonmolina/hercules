ef2dbfb9b932625993b34aa53a098d57ac66aac7
nilsonmolina <nilson13@me.com>
2018-05-04 11:09:58 -0700

    added option to generate key or not

--- 10-apples-of-hesperides/apples.sh
+++ 10-apples-of-hesperides/apples.sh
@@ -4,13 +4,25 @@ if [ "$#" -ne 3 ] ; then
     echo "\nEXAMPLE: \n - user: hercules\n - ip:   localhost\n - port: 1313"
     exit 1
 fi
-# Generate SSH Key | -t Type | -f Filename | -q Silence | -N New Password
-clear; echo "\n ------------ Generating RSA Key -----------"
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
-echo "\n -- RSA key created!"
-# Copy Public Key
-echo "\n ---------- Copying RSA Key to VM ----------\n    (Password is required only this one time)\n"
-ssh-copy-id -p $3 $1@$2
+# Prompt to create key
+while true; do
+    read -p "Do you want to generate an RSA key? (y/n)  " yn
+    case $yn in 
+    [Yy]* )
+            # Generate SSH Key | -t Type | -f Filename | -q Silence | -N New Password
+            clear; echo "\n ------------ Generating RSA Key -----------"
+            ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N "" && echo "\n -- RSA key created!"
+            # Copy Public Key
+            echo "\n ---------- Copying RSA Key to VM ----------\n    (Password is required only this one time)\n"
+            ssh-copy-id -i ~/.ssh/id_rsa -p $3 $1@$2
+            break;;
+    [Nn]* ) 
+            break;;
+    *) 
+            echo "Please enter yes or no.\n";;
+    esac
+done
+
 # Connect to VM using ssh and RSA Key & Create Bonus Repo
 echo "\n ----- Connecting & Creating Bonus Repo -----";
 ssh -p $3 $1@$2 "mkdir ~/bonus && git init ~/bonus"
