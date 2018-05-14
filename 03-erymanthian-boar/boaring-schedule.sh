1684436364ce9aa01c10bd1895eee514ff7501e7
nilsonmolina <nilson13@me.com>
2018-05-13 20:10:54 -0700

    sanitized boar

--- 03-erymanthian-boar/boaring-schedule.sh
+++ 03-erymanthian-boar/boaring-schedule.sh
@@ -2,4 +2,4 @@
 # On macOS, enable atrun utility with the following code: 
 # sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.atrun.plist
 
-echo "echo 'oink oink mother f*cker!' >> boar.txt | open boar.txt" | at 08:42 AM 12/21/2018
+echo "echo 'oink oink!' >> boar.txt | open boar.txt" | at 08:42 AM 12/21/2018
